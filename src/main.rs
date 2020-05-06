7968fdc037af77265da534b87b49c3a6f8363428
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-05-06 00:02:03 -0400

    Added delta time and switched to shape objects to use floating point locations

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,47 @@
 use raylib::prelude::*;
 
-const PLAYER_HEIGHT: i32 = 40;
-const PLAYER_WIDTH: i32 = 10;
-const PLAYER_HEAD_RADIUS: i32 = 10;
-const PLAYER_HEAD_FUDGE: i32 = 5;
+const PLAYER_HEIGHT: f32 = 40.0;
+const PLAYER_WIDTH: f32 = 10.0;
+const PLAYER_HEAD_RADIUS: f32 = 10.0;
+const PLAYER_HEAD_FUDGE: f32 = 20.0;
+const PLAYER_SPEED: f32 = 150.0;
 
 fn main() {
     use raylib::consts::KeyboardKey::*;
 
-    let (mut rl, thread) = raylib::init().size(640, 480).title("Hello, World").build();
+    let (mut rl, thread) = raylib::init().size(640, 480).title("Shrubbery").build();
 
-    let mut x: i32 = 50;
-    let mut y: i32 = 50;
+    let mut x: f32 = 50.0;
+    let mut y: f32 = 50.0;
+    let mut player = Rectangle::new(x, y, PLAYER_WIDTH, PLAYER_HEIGHT);
+    let mut player_head_center = Vector2::new(x, y - PLAYER_HEIGHT / 2.0 - PLAYER_HEAD_RADIUS + PLAYER_HEAD_FUDGE);
+
+    let mut delta_time = rl.get_frame_time();
 
     rl.set_target_fps(60);
     while !rl.window_should_close() {
+        delta_time = rl.get_frame_time();
+
         if rl.is_key_down(KEY_A) {
-            x -= 1;
+            player.x -= PLAYER_SPEED * delta_time;
         } else if rl.is_key_down(KEY_D) {
-            x += 1;
+            player.x += PLAYER_SPEED * delta_time;
         }
         if rl.is_key_down(KEY_W) {
-            y -= 1;
+            player.y -= PLAYER_SPEED * delta_time;
         } else if rl.is_key_down(KEY_S) {
-            y += 1;
+            player.y += PLAYER_SPEED * delta_time;
         }
+
+        player_head_center.x = player.x;
+        player_head_center.y = player.y - PLAYER_HEIGHT / 2.0 - PLAYER_HEAD_RADIUS + PLAYER_HEAD_FUDGE;
 
         let mut d = rl.begin_drawing(&thread);
 
         d.clear_background(Color::WHITE);
-        d.draw_rectangle(
-            x - PLAYER_WIDTH / 2,
-            y - PLAYER_HEIGHT / 2,
-            PLAYER_WIDTH,
-            PLAYER_HEIGHT,
-            Color::VIOLET,
-        );
-        d.draw_circle(
-            x,
-            y - PLAYER_HEIGHT / 2 - PLAYER_HEAD_RADIUS + PLAYER_HEAD_FUDGE,
+        d.draw_rectangle_rec(&player, Color::VIOLET);
+        d.draw_circle_v(
+            player_head_center,
             PLAYER_HEAD_RADIUS as f32,
             Color::LIME,
         );
