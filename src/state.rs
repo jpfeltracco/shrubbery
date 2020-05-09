dba5a2159c1c31b67b48c0a6d0c9064d3bc32566
Mitch McClellan <mitchelljmcclellan@gmail.com>
2020-05-09 16:49:50 -0400

    sprite loading

--- src/state.rs
+++ src/state.rs
@@ -2,7 +2,11 @@ use amethyst::{
     core::transform::Transform,
     input::{is_key_down, VirtualKeyCode},
     prelude::*,
-    renderer::Camera,
+    assets::{AssetStorage, Loader},
+    renderer::{
+        Camera, ImageFormat, SpriteSheetFormat,
+        SpriteSheet, SpriteRender, Texture, Transparent,
+    },
     window::ScreenDimensions,
 };
 
@@ -15,6 +19,10 @@ impl SimpleState for MyState {
         let dimensions = (*world.read_resource::<ScreenDimensions>()).clone();
 
         init_camera(world, &dimensions);
+
+        // Assets
+        let sprites = load_sprites(world);
+        init_sprites(world, &sprites, &dimensions);
     }
     fn handle_event(
         &mut self,
@@ -42,4 +50,61 @@ fn init_camera(world: &mut World, dimensions: &ScreenDimensions) {
         .with(Camera::standard_2d(dimensions.width(), dimensions.height()))
         .with(transform)
         .build();
+}
+
+fn load_sprites(world: &mut World) -> Vec<SpriteRender> {
+    // Load the texture for our sprites. We'll later need to
+    // add a handle to this texture to our `SpriteRender`s, so
+    // we need to keep a reference to it.
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(
+            "sprites/knight_sprite_sheet.png",
+            ImageFormat::default(),
+            (),
+            &texture_storage,
+        )
+    };
+
+    // Load the spritesheet definition file, which contains metadata on our
+    // spritesheet texture.
+    let sheet_handle = {
+        let loader = world.read_resource::<Loader>();
+        let sheet_storage = world.read_resource::<AssetStorage<SpriteSheet>>();
+        loader.load(
+            "sprites/knight_sprite_sheet.ron",
+            SpriteSheetFormat(texture_handle),
+            (),
+            &sheet_storage,
+        )
+    };
+
+    // Create our sprite renders. Each will have a handle to the texture
+    // that it renders from. The handle is safe to clone, since it just
+    // references the asset.
+    (0..0)
+        .map(|i| SpriteRender {
+            sprite_sheet: sheet_handle.clone(),
+            sprite_number: i,
+        })
+        .collect()
+}
+
+fn init_sprites(world: &mut World, sprites: &[SpriteRender], dimensions: &ScreenDimensions) {
+    for (i, sprite) in sprites.iter().enumerate() {
+        // Center our sprites around the center of the window
+        let x = (i as f32 - 1.) * 100. + dimensions.width() * 0.5;
+        let y = (i as f32 - 1.) * 100. + dimensions.height() * 0.5;
+        let mut transform = Transform::default();
+        transform.set_translation_xyz(x, y, 0.);
+
+        // Create an entity for each sprite and attach the `SpriteRender` as well as the transform.
+        world
+            .create_entity()
+            .with(sprite.clone())
+            .with(transform)
+            .with(Transparent)
+            .build();
+    }
 }
